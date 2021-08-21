157449146c041b506a301ccf54d2873bb98ae729
justin <>
2021-08-21 22:30:26 +0800

    feat: add row component

---
+++ src/components/Row/index.tsx
@@ -0,0 +1,42 @@
+import React from "react";
+import styled from "@emotion/styled";
+
+ const Row = styled.div<{
+  gap?: number | boolean;
+  between?: boolean;
+  marginBottom?: number;
+}>`
+  display: flex;
+  align-items: center;
+  justify-content: ${(props) => (props.between ? "space-between" : undefined)};
+  margin-bottom: ${(props) => props.marginBottom + "rem"};
+  > * {
+    margin-top: 0 !important;
+    margin-bottom: 0 !important;
+    margin-right: ${(props) =>
+            typeof props.gap === "number"
+                    ? props.gap + "rem"
+                    : props.gap
+                    ? "2rem"
+                    : undefined};
+  }
+`;
+export default Row;
+
+const Container = styled.div`
+  display: grid;
+  grid-template-rows: 6rem 1fr;
+  height: 100vh;
+`;
+
+const Header = styled(Row)`
+  padding: 3.2rem;
+  box-shadow: 0 0 5px 0 rgba(0, 0, 0, 0.1);
+  z-index: 1;
+`;
+const HeaderLeft = styled(Row)``;
+const HeaderRight = styled.div``;
+const Main = styled.main`
+  display: flex;
+  overflow: hidden;
+`;
