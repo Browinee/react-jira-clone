d58649158f44f60e8b03e2d912bf017c1c5e5cb5
justin <>
2021-08-22 21:13:55 +0800

    feat: add http

--- src/utils/http.ts
+++ src/utils/http.ts
@@ -1,5 +1,7 @@
 import * as auth from "../module/auth/auth-provider";
 import { useAuth } from "../context/auth-context";
+import { useCallback } from "react";
+import qs from "qs";
 const apiUrl = process.env.REACT_APP_API_URL;
 
 
@@ -30,14 +32,22 @@ export const http = (endpoint: string, { data, token, headers, ...customConfig }
     },
     ...customConfig,
   };
-
+  if (config.method.toUpperCase() === "GET") {
+    endpoint += `?${qs.stringify(data)}`;
+  } else {
+    config.body = JSON.stringify(data || {});
+  }
   return window.fetch(`${apiUrl}/${endpoint}`, config)
     .then(handleResponse);
 };
 
 
 export const useHttp = () => {
-  const user = useAuth();
-
+  const {user} = useAuth();
+  return useCallback(
+    (...[endpoint, config]: Parameters<typeof http>) =>
+      http(endpoint, { ...config, token: user?.token }),
+    [user?.token]
+  );
 }
 
