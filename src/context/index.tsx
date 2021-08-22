767c7c5a01e659ea1ceefd09ad81f35c192ff467
justin <>
2021-08-22 19:20:09 +0800

    feat: add react-query and jira-dev-tool@next

--- src/context/index.tsx
+++ src/context/index.tsx
@@ -1,8 +1,20 @@
 import { ReactNode } from "react";
 import { AuthProvider } from "./auth-context";
+import { QueryClient, QueryClientProvider } from "react-query";
 
-export const AppProvider = ({children}: {children: ReactNode}) => {
-  return <AuthProvider>
-    {children}
-  </AuthProvider>
-}
+export const AppProvider = ({ children }: { children: ReactNode }) => {
+  const queryClient = new QueryClient({
+    defaultOptions: {
+      queries: {
+        refetchOnWindowFocus: false
+      }
+    }
+  });
+  return (
+    <QueryClientProvider client={queryClient}>
+      <AuthProvider>
+        {children}
+      </AuthProvider>
+
+    </QueryClientProvider>);
+};
--- src/index.tsx
+++ src/index.tsx
@@ -3,15 +3,16 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
-import { loadDevTools } from "jira-dev-tool";
+import { DevTools, loadServer } from "jira-dev-tool";
 // put it behind jira-dev-tool, then our customized theme will override default theme in jira-dev-tool
 import 'antd/dist/antd.less'
 import { AppProvider } from "./context";
 
-loadDevTools(() => {
+loadServer(() => {
   ReactDOM.render(
     <React.StrictMode>
       <AppProvider>
+        <DevTools />
         <App />
       </AppProvider>
     </React.StrictMode>,
