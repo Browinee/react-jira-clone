567818849e95f6a03fbbad05f918274709a53aff
justin.lin <justin.lin@yxunistar.com>
2021-08-23 16:18:52 +0800

    feat: add global loading

--- src/context/auth-context.tsx
+++ src/context/auth-context.tsx
@@ -1,9 +1,12 @@
-import React, { ReactNode, useContext, useState } from "react";
+import React, { ReactNode, useCallback, useContext, useState } from "react";
 import * as auth from "../module/auth/auth-provider";
 import { AuthForm } from "../module/auth/auth-provider";
 import { http } from "../utils/http";
 import useMount from "../hook/useMount";
 import { User } from "../types/user";
+import useAsync from "../hook/useAsync";
+import Loading from "../components/Loading";
+import FullPageErrorFallback from "../components/FullPageErrorFallback";
 
 interface AuthContext {
   user: User | null;
@@ -11,7 +14,6 @@ interface AuthContext {
   register: (form: AuthForm) => void;
   logout: () => void;
 }
-
 
 const bootstrapUser = async () => {
   let user = null;
@@ -27,15 +29,35 @@ const AuthContext = React.createContext<AuthContext | undefined>(undefined);
 AuthContext.displayName = "AuthContext";
 
 export const AuthProvider = ({ children }: { children: ReactNode }) => {
-  const [user, setUser] = useState<null | User>(null);
+  const {
+    data: user,
+    error,
+    isLoading,
+    isIdle,
+    isError,
+    run,
+    setData: setUser,
+  } = useAsync<User | null>();
   const login = (form: AuthForm) => auth.login(form).then(setUser);
   const register = (form: AuthForm) => auth.register(form).then(setUser);
   const logout = () => auth.logout().then(() => setUser(null));
-  useMount(bootstrapUser);
+  useMount(
+    useCallback(() => {
+      run(bootstrapUser());
+    }, [])
+  );
+  if (isIdle || isLoading) {
+    return <Loading />;
+  }
 
-  return <AuthContext.Provider value={{ user, login, register, logout }}>
-    {children}
-  </AuthContext.Provider>;
+  if (isError) {
+    return <FullPageErrorFallback error={error} />;
+  }
+  return (
+    <AuthContext.Provider value={{ user, login, register, logout }}>
+      {children}
+    </AuthContext.Provider>
+  );
 };
 
 export const useAuth = () => {
